68c3ca46194fb3f436133233050130361b9299dd
Franck Boirot <franck.brt@hotmail.fr>
2022-04-28 16:49:23 +0200

    bdd table childrens

---
+++ src/Entity/Childrens.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChildrensRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChildrensRepository::class)]
+class Childrens
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $additional;
+
+    #[ORM\Column(type: 'string', length: 20)]
+    private $age_range;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAdditional(): ?string
+    {
+        return $this->additional;
+    }
+
+    public function setAdditional(?string $additional): self
+    {
+        $this->additional = $additional;
+
+        return $this;
+    }
+
+    public function getAgeRange(): ?string
+    {
+        return $this->age_range;
+    }
+
+    public function setAgeRange(string $age_range): self
+    {
+        $this->age_range = $age_range;
+
+        return $this;
+    }
+
+
+}
