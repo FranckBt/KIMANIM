dca287698fa5191da0cd5d45a70c4e2be87c69e3
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-04-29 14:51:16 +0200

    Activity fixture

--- src/DataFixtures/ActivitiesFixtures.php
+++ src/DataFixtures/ActivitiesFixtures.php
@@ -1,43 +1,33 @@
 <?php
-
 namespace App\DataFixtures;
 
 use App\Entity\Activities;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class ActivitiesFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR'); // create a French faker
+        $activities = ["Aller en campagne", "Aller à la plage", "Aller à la pêche"];
 
-        $activity1 = new Activities();
-        $activity1
-            ->setStartOn(new DateTimeImmutable('2022-08-12'))
-            ->setAdress('rue de la brossollette')
-            ->setCity('Bordeaux')
-            ->setZipcode(33000)
-            ->setDescription('Activité en bord de mer')
-            ->setStatus('A venir')
-            ->setMinParticipants(5);
+        for ($i = 0; $i < 100; $i++) {
+            $activity = new Activities();
+            $activity
+                ->setStartOn(new DateTimeImmutable($faker->date))
+                ->setAdress($faker->streetAddress)
+                ->setCity($faker->city)
+                ->setZipcode($faker->postcode)
+                ->setDescription($activities[array_rand($activities,1)])
+                ->setStatus($faker->randomElement(['Projet', 'Animateur requis', 'Confirmé', 'Annulé', 'Terminé']))
+                ->setMinParticipants($faker->numberBetween(1, 10));
+                
+            $manager->persist($activity);
 
-        $manager->persist($activity1);
-
-        $activity2 = new Activities();
-        $activity2
-            ->setStartOn(new DateTimeImmutable('2022-09-10'))
-            ->setAdress('20 allée du moulin vert')
-            ->setCity('Marseille')
-            ->setZipcode(13000)
-            ->setDescription('Visite d\'un bateau')
-            ->setStatus('A venir')
-            ->setMinParticipants(10);
-
-        $manager->persist($activity2);
-
+        }
         $manager->flush();
     }
 }
