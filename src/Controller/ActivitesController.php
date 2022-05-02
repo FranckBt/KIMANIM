06538ce046d7503a3394594c3b2222cc2a90e917
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-05-02 16:39:54 +0200

    modif Route par défaut

--- src/Controller/ActivitesController.php
+++ src/Controller/ActivitesController.php
@@ -3,15 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Activities;
+use App\Form\ActivityFrontType;
 use App\Repository\ActivitiesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route ('/activites')]
 class ActivitesController extends AbstractController
 {
 
-    #[Route('/activites', name: 'app_activites', methods: ['GET'])]
+    #[Route('/', name: 'app_activites', methods: ['GET'])]
     public function index(ActivitiesRepository $activityRepository): Response
     {
         return $this->render('activites/index.html.twig', [
