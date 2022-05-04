b0a78577c87e726f5a704cccb4b69d84860eb3b6
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-05-04 09:14:16 +0200

    Nouvelle migration

--- migrations/Version20220503145228.php
+++ migrations/Version20220503145228.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220503141514 extends AbstractMigration
+final class Version20220503145228 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,7 +23,7 @@ final class Version20220503141514 extends AbstractMigration
         $this->addSql('CREATE TABLE activities (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, description LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', start_on DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', adress LONGTEXT NOT NULL, city VARCHAR(255) NOT NULL, zipcode VARCHAR(255) NOT NULL, status VARCHAR(20) NOT NULL, min_participants INT NOT NULL, title VARCHAR(255) NOT NULL, INDEX IDX_B5F1AFE5A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE avis (id INT AUTO_INCREMENT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', text LONGTEXT DEFAULT NULL, rate INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE childrens (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, additional LONGTEXT DEFAULT NULL, age_range VARCHAR(20) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, surname VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', name VARCHAR(255) DEFAULT NULL, actif TINYINT(1) DEFAULT NULL, validate TINYINT(1) DEFAULT NULL, phone VARCHAR(30) DEFAULT NULL, alias VARCHAR(255) DEFAULT NULL, token VARCHAR(255) DEFAULT NULL, token_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', avatar VARCHAR(255) DEFAULT NULL, bio LONGTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, surname VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', name VARCHAR(255) DEFAULT NULL, actif TINYINT(1) DEFAULT NULL, validate TINYINT(1) DEFAULT NULL, phone VARCHAR(30) DEFAULT NULL, alias VARCHAR(255) DEFAULT NULL, token VARCHAR(255) DEFAULT NULL, token_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', avatar VARCHAR(255) DEFAULT NULL, bio LONGTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE activities ADD CONSTRAINT FK_B5F1AFE5A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
     }
 
