7c13905b20af6a723e3ca06ed50ced8dc199f6fe
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-05-05 10:36:36 +0200

    Corrections sur fixtures

--- src/DataFixtures/AvisFixtures.php
+++ src/DataFixtures/AvisFixtures.php
@@ -17,7 +17,7 @@ class AvisFixtures extends Fixture
         for ($i = 0; $i < 10; $i++) {
             $avis = new Avis();
             $avis
-                ->setText($faker->text(100))
+                ->setText('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non consequat quam. Aenean interdum diam a varius lobortis. Nunc at lacus vel arcu blandit aliquet non a felis. Maecenas in egestas felis. Nullam fringilla arcu sed metus tempor, quis euismod leo ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla a imperdiet turpis, nec ultricies risus. Nunc non molestie arcu. Fusce rhoncus mauris massa, at accumsan risus luctus eget. Aliquam nec placerat leo. Pellentesque consequat sem vel mi semper, nec tempor nunc aliquam. Donec lectus purus, congue et tempor ac, pulvinar non ex. Pellentesque.')
                 ->setCreatedAt(new DateTimeImmutable($faker->date))
                 ->setRate($faker->numberBetween(0, 5));
             $manager->persist($avis);
--- src/DataFixtures/ChildrensFixtures.php
+++ src/DataFixtures/ChildrensFixtures.php
@@ -17,7 +17,7 @@ class ChildrensFixtures extends Fixture
             $child = new Childrens();
             $child
                 ->setName($faker->firstName)
-                ->setAdditional($faker->text(100))
+                ->setAdditional('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non consequat quam. Aenean interdum diam a varius lobortis. Nunc at lacus vel arcu blandit aliquet non a felis. Maecenas in egestas felis. Nullam fringilla arcu sed metus tempor, quis euismod leo ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla a imperdiet turpis, nec ultricies risus. Nunc non molestie arcu. Fusce rhoncus mauris massa, at accumsan risus luctus eget. Aliquam nec placerat leo. Pellentesque consequat sem vel mi semper, nec tempor nunc aliquam. Donec lectus purus, congue et tempor ac, pulvinar non ex. Pellentesque.')
                 ->setAgeRange($faker->randomElement(['4-6 ans', '6-8 ans', '8-12 ans', '12-16 ans']));
             $manager->persist($child);
         }
