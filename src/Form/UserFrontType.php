83c3595091dccf4b5fb9fdd933ae365c2fc199fa
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-05-06 18:41:49 +0200

    Ajout upload avatar formulaire

--- src/Form/UserFrontType.php
+++ src/Form/UserFrontType.php
@@ -4,10 +4,12 @@ namespace App\Form;
 
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class UserFrontType extends AbstractType
 {
@@ -36,6 +38,29 @@ class UserFrontType extends AbstractType
             ->add('bio', TextareaType::class, [
                 'label' => FALSE,
                 'required' => FALSE,
+            ])
+            ->add('avatar', FileType::class, [
+                'label' => 'Avatar',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Seules les images au format .jpeg, .jpg et .png sont acceptées',
+                    ])
+                ],
             ]);
     }
 
